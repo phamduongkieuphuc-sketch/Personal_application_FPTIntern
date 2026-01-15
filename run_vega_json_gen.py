f07f302eaa5e89ca80019933cf83dd0604665232
phamduongkieuphuc-sketch <phamduongkieuphuc@gmail.com>
2026-01-15 08:55:54 +0700

    Drawing population bubble chart using plotly. Output html response and display it on web. Image is also saved locally

--- run_vega_json_gen.py
+++ run_vega_json_gen.py
@@ -25,12 +25,15 @@ def send_chart_request(data, question):
 
 if __name__ == "__main__":
     data = [
-        {"product_name": "thit lon", "revenue": 10000000}, {"product_name": "thit bo", "revenue": 7600000}, {"product_name": "thit ga", "revenue": 2000000},
-      {"product_name": "thit cuu", "revenue": 3000000}, {"product_name": "thit kangaroo", "revenue": 1000000},
+        {"province": "Hà Nội", "lat": 21.0285, "lon": 105.8542, "population": 8400000},
+                {"province": "TP.HCM", "lat": 10.8231, "lon": 106.6297, "population": 9300000},
+                {"province": "Đà Nẵng", "lat": 16.0544, "lon": 108.2022, "population": 1200000},
+                {"province": "Hải Phòng", "lat": 20.8449, "lon": 106.6881, "population": 2100000},
+                {"province": "Cần Thơ", "lat": 10.0452, "lon": 105.7469, "population": 1250000},
     ]
     question = "Top 3 san pham co doanh thu cao nhat"
 
-    question_1 = "Ty le doanh thu cac san pham trong thang truoc bang bieu do tron"
+    question_1 = "Tao bieu do dan so bubble chart"
 
 
     result = send_chart_request(data, question_1)
